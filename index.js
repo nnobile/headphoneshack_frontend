f876778bc306ecf521c8be05dc5b0bd410354bde
Nick Nobile <nickcnobile@gmail.com>
2021-04-22 00:45:20 -0400

    create getHeadphones functionto call index API endpoint

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("in index.js");
+const endPoint = "http://127.0.0.1:3000/api/v1/headphones"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getHeadphones()
+    })
+
+    function getHeadphones(){
+        fetch (endPoint)
+        .then(response => response.json())
+        .then(headphones => {
+        console.log(headphones);
+        
+    })
+  }   
