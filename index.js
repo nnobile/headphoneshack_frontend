56b66719261a44c30084cab74726566d20c66c27
Nick Nobile <nickcnobile@gmail.com>
2021-04-22 01:34:14 -0400

    finish get function which makes call to index API endpoint

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const endPoint = "http://127.0.0.1:3000/api/v1/headphones"
 
 document.addEventListener('DOMContentLoaded', () => {
     getHeadphones()
-    })
+})
 
-    function getHeadphones(){
-        fetch (endPoint)
-        .then(response => response.json())
-        .then(headphones => {
-        console.log(headphones);
-        
+function getHeadphones() {
+    fetch(endPoint)
+      .then(res => res.json())
+      .then(headphones => {
+        headphones.data.forEach(headphone => {
+          const headphoneMarkup = `
+            <div data-id=${headphone.id}>
+            <h3>${headphone.attributes.brand}</h3>
+            <p>${headphone.attributes.model}</p>
+            <p>${headphone.attributes.description}</p>
+            <p>$${headphone.attributes.price}</p>
+            <p>${headphone.attributes.category.name}</p>
+            <button data-id=${headphone.id}>Edit</button>
+            </div>
+          <br><br>`;
+
+          document.querySelector('#headphone-container').innerHTML += headphoneMarkup
+      })
     })
-  }   
+}
