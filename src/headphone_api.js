7bdfee4f971ae215be4f906da4f087b22a65f744
Nick Nobile <nickcnobile@gmail.com>
2021-05-10 17:32:51 -0400

    refactor index and headphone api class

--- src/headphone_api.js
+++ src/headphone_api.js
@@ -13,4 +13,22 @@ class HeadphoneApi{
       })
     }
 
+    static postFetch (brand, model, description, price, category_id) {
+      const bodyData = {brand, model, description, price, category_id}
+      fetch(HeadphoneApi.endPoint, {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(bodyData)
+      })
+      .then(response => response.json())
+      .then(headphone => {
+          const headphoneData = headphone.data
+          const newHeadphone = new Headphone(headphoneData, headphoneData.attributes)
+          const headphoneCard = document.createElement('div');
+          headphoneCard.innerHTML= newHeadphone.renderHeadphoneCard()
+          document.querySelector('#headphone-container').prepend(headphoneCard)            
+      })
+      .catch(error => console.log(error))
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    getCategories()
-    getHeadphones()
+    CategoryApi.getCategories()
+    HeadphoneApi.getHeadphones()
 
     const createHeadphoneForm = document.querySelector("#create-headphone-form")
 
@@ -8,47 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-function getHeadphones() {
-    HeadphoneApi.getHeadphones()
-}
-
-function getCategories() {
-    CategoryApi.getCategories()
-}
-
-
-function createFormHandler(e) {
-    e.preventDefault()
+function createFormHandler(event) {
+    event.preventDefault()
     const brandInput = document.querySelector('#input-brand').value
     const modelInput = document.querySelector('#input-model').value
     const descriptionInput = document.querySelector('#input-description').value
     const priceInput = document.querySelector('#input-price').value
     const categoryId = parseInt(document.querySelector('#categories').value)
-    postFetch(brandInput, modelInput, descriptionInput, priceInput, categoryId)
+    
+    HeadphoneApi.postFetch(brandInput, modelInput, descriptionInput, priceInput, categoryId)
     document.querySelector('#input-brand').value = "";
     document.querySelector('#input-model').value = "";
     document.querySelector('#input-description').value = "";
     document.querySelector('#input-price').value = "";
-
 }
 
-function postFetch (brand, model, description, price, category_id) {
-    const bodyData = {brand, model, description, price, category_id}
-    fetch(HeadphoneApi.endPoint, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(bodyData)
-    })
-    .then(response => response.json())
-    .then(headphone => {
-        const headphoneData = headphone.data
-        const newHeadphone = new Headphone(headphoneData, headphoneData.attributes)
-        const headphoneCard = document.createElement('div');
-        headphoneCard.innerHTML= newHeadphone.renderHeadphoneCard()
-        document.querySelector('#headphone-container').prepend(headphoneCard)            
-    })
-    .catch(error => console.log(error))
-}
+
 
 
 
